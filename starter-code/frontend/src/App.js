37e702455380228001d7e710c142061a3aa3e7ce
areejassfour <areej.assfour@gmail.com>
2022-06-30 19:54:53 +0300

    home

--- starter-code/frontend/src/App.js
+++ starter-code/frontend/src/App.js
@@ -1,8 +1,30 @@
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Navbar1 from "./Navbar";
+import Home from "./Home";
+import About from "./About";
+
+
+
+
+
+
+
 
 function App() {
-  return <div>
-    <h1>hhhhh</h1>
+  return <div className="App">
+  
+   <Navbar1/>
+  
+   <Routes>
+        <Route path="/home" element={<Home/>} />
+        <Route path="/about" element={<About/>} />
+
+        
+
+        
+    
+      </Routes>
   </div>;
 }
 

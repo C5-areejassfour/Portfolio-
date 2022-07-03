54ba205faf88442ff08537bd65404b9869586b1e
areejassfour <areej.assfour@gmail.com>
2022-07-03 16:38:20 +0300

    style

--- starter-code/frontend/src/App.js
+++ starter-code/frontend/src/App.js
@@ -3,8 +3,10 @@ import { Route, Routes } from "react-router-dom";
 import Navbar1 from "./Navbar";
 import Home from "./Home";
 import About from "./About";
-
-
+import Skills from "./Skills";
+import Project from "./Project";  
+ 
+import Contact from "./Contact";
 
 
 
@@ -19,10 +21,10 @@ function App() {
    <Routes>
         <Route path="/home" element={<Home/>} />
         <Route path="/about" element={<About/>} />
+        <Route path="/skills" element={<Skills/>} />
+        <Route path="/project" element={<Project/>} />
+        <Route path="/contact" element={<Contact/>} />
 
-        
-
-        
     
       </Routes>
   </div>;

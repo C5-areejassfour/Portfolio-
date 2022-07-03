7c7430837ab8d672861605359209a1e455c2bba4
areejassfour <areej.assfour@gmail.com>
2022-07-03 16:37:00 +0300

    about

--- starter-code/frontend/src/About/index.js
+++ starter-code/frontend/src/About/index.js
@@ -1,36 +1,41 @@
 import "./style.css";
-import Card from 'react-bootstrap/Card';
+import Card from "react-bootstrap/Card";
 
-const About= ()=>{
-    //I am a full stack web developer who can work with a team and has the ability to continuously learn
-return (
+const About = () => {
+  //I am a full stack web developer who can work with a team and has the ability to continuously learn
+  return (
     <div className="mm">
-        <div className="main_about">
-        <h2 className="maina"><p>About Me</p></h2>
-
-        <p className="pa1">I am a full stack web developer who can work with a team and has the ability to continuously learn</p> 
-        <h2><p className="pa1">Education</p></h2>
-       
+      <div className="main_about">
         <Card>
-  <Card.Header>Education</Card.Header>
-  <Card.Body>
-    <blockquote className="blockquote mb-0">
-    <p className="pa1">Bootcamp in programming (FULL-STACK WEB DEVELOPER) at the MERAKI ACADEMY</p>
-        <p className="pa1">6-March-2022 to 3-August-2022</p>
-        <p className="pa1">Bachelor of  Civil Engineering </p>
-        <p className="pa1">AL-Hussen Bin-Talal University</p>
-        <p className="pa1">2013 - 2018</p>
-      <footer className="blockquote-footer">
-        
-      </footer>
-    </blockquote>
-  </Card.Body>
-</Card>
-        </div>
-     
+          <Card.Header className="pa12">About Me</Card.Header>
+          <Card.Body>
+            <blockquote className="blockquote mb-0">
+              <p className="pa1">
+                I am a full stack web developer who can work with a team and has
+                the ability to continuously learn
+              </p>
+            </blockquote>
+          </Card.Body>
+        </Card>
 
-
+        <Card>
+          <Card.Header className="pa12" >Education</Card.Header>
+          <Card.Body>
+            <blockquote className="blockquote mb-0">
+              <p className="pa1">
+                Bootcamp in programming (FULL-STACK WEB DEVELOPER) at the MERAKI
+                ACADEMY
+              </p>
+              <p className="pa1">6-March-2022 to 3-August-2022</p>
+              <p className="pa1">Bachelor of Civil Engineering </p>
+              <p className="pa1">AL-Hussen Bin-Talal University</p>
+              <p className="pa1">2013 - 2018</p>
+              
+            </blockquote>
+          </Card.Body>
+        </Card>
+      </div>
     </div>
-)
-}
+  );
+};
 export default About;

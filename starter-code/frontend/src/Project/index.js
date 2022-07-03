5c252beaa020c265939330dd897dece31096d5ce
areejassfour <areej.assfour@gmail.com>
2022-07-04 02:15:37 +0300

    style

--- starter-code/frontend/src/Project/index.js
+++ starter-code/frontend/src/Project/index.js
@@ -1,9 +1,21 @@
-
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
 
 const Project =()=>{
     return(
         <div>
-            <h1>Project</h1>
+<Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="holder.js/100px180" />
+  <Card.Body>
+    <Card.Title>Card Title</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+  </Card.Body>
+</Card>
             
         </div>
     )
